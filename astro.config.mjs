7c2bcc1e92ed8581cbfa89b8c4e92743c9a222eb
Tonho <antonioreverso.com@gmail.com>
2025-02-19 22:27:34 -0300

    Add base URL configuration for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import vue from '@astrojs/vue';
 
 export default defineConfig({
     site: 'https://antonioreverso.github.io',
+    base: 'https://antonioreverso.github.io',
     integrations: [
     theme({
         site: {
